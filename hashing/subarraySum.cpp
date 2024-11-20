7eaf74e709f9f3afa9135536a5af0b9ab1796fbf
vanshika705 <vnshkjngd@gmail.com>
2024-11-20 19:55:15 +0530

    dsa

---
+++ hashing/subarraySum.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+// brute approch
+// int subarraySum(vector<int>& vec, int k ){ // tc = O(n ^ 2) , sc = O(1) 
+//     int count = 0;
+//     int n = vec.size();
+
+//     for(int i = 0 ; i< n ; i++){ // starting point
+//         int sum =0 ;
+
+//         for(int j = i ; j<n ; j++){ // endling point
+//             sum += vec[j] ;
+
+//             if(sum == k ){
+//                 count ++ ;
+//             }
+//         }
+//     }
+    
+//     return count;
+// } 
+
+// optimal approch 
+int subarraySum(vector<int>& vec, int k ){  // O(n) ( tc and sc)
+    int count = 0;  
+    int n = vec.size();
+
+    vector<int> prefixSum(n,0);
+
+    prefixSum[0] = vec[0];
+
+    for(int i = 0 ; i< n ; i++){
+        prefixSum[i] = prefixSum[i-1] + vec[i];
+    }
+
+    unordered_map<int,int> m ; //ps , frequency
+
+    for(int j = 0 ; j<n ; j++){
+        if(prefixSum[j] == k) count ++ ;
+
+        int val = prefixSum[j] - k ; // ps[i-1]
+        if(m.find[val] != m.end()){ // exist karti he
+            count += m[val];
+        }
+
+        if(m.find[prefixSum[j]] == m.end()){ // not exist
+            m[prefixSum[j]] = 0;
+        }
+        m[prefixSum[j]] ++ ;
+    }
+    
+    return count;
+}
+
+int main(){
+    vector<int> vec = {9,4,20,3,10,5};
+
+    int target= 33;
+
+    for(int val:vec){
+        cout << val << " ";
+    }
+    cout << endl;
+
+    cout<< "total sub arrays are : " << subarraySum(vec, target);
+    
+
+    return 0;
+}
--- hashing/tempCodeRunnerFile.cpp
+++ hashing/tempCodeRunnerFile.cpp
@@ -1,60 +1,39 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <unordered_map>
 using namespace std;
 
-vector<vector<int>> fourSum(vector<int>& vec , int target ){
-    vector<vector<int>> ans ;
+int subarraySum(vector<int>& vec, int k ){
+    int count = 0;
     int n = vec.size();
 
-    sort(vec.begin(),vec.end());
+    for(int i = 0 ; i< n ; i++){
+        int sum =0 ;
 
-    for(int i =0 ; i<n ; i++){
-        if(i>0 && vec[i] == vec[i-1]) continue ; // skips duplicate values
+        for(int j = i ; j<n ; j++){
+            sum += vec[j] ;
 
-        for(int j =i+1 ; j<n ;){
-            int p = j+1, q = n-1;
-
-            while(p<q){
-                long long sum = (long long)vec[i] +(long long)vec[j] +(long long)vec[p] +(long long)vec[q] ;
-
-                if(sum<target ){
-                    p++;
-                }else if(sum > target ){
-                    q--;
-                }else {
-                    // sum == target 
-                    ans.push_back({vec[i], vec[j], vec[p],vec[q]});
-                    p++; q--;
-
-                    while(p<q && vec[p] == vec[p-1]) p++ ; // skips duplicate values
-                }
-            } 
-            j++ ; 
-            while(j<n && vec[j] == vec[j-1]) j++ ; // skips duplicate values
+            if(sum == k ){
+                count ++ ;
+            }
         }
     }
-   
-    return ans;
+    
+    return count;
 }
 
 int main(){
-    vector<int> vec = {-2,-1,-1,1,1,2,2};
-    int target = 0;
+    vector<int> vec = {9,4,20,3,10,5};
+
+    int target= 33;
 
     for(int val:vec){
         cout << val << " ";
     }
     cout << endl;
 
-    cout<< "elements of 4 sum pair is : ";
-    for(const auto& row :fourSum(vec, target)){
-            for(const auto& element : row){
-            cout << element << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
+    cout<< "total sub arrays are : " << subarraySum(vec, target);
+    
 
     return 0;
 }
