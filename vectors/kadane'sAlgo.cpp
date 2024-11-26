6410f91e72e4fe908ee95d534f3da27e5855c1a5
vanshika705 <vnshkjngd@gmail.com>
2024-11-26 20:35:37 +0530

    DSA

--- vectors/kadane'sAlgo.cpp
+++ vectors/kadane'sAlgo.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <limits>
 using namespace std;
 
 int sumOfSubarrays(vector<int>& nums){
-    int maxSum =0; 
+    int maxSum = 0; 
     int currentSum =0;
 
     for (int val: nums){
@@ -14,20 +15,11 @@ int sumOfSubarrays(vector<int>& nums){
             currentSum = 0;
         }
         
-        cout << endl;
     }
     return maxSum;
 }
 int main(){
-    vector<int> nums;
-    nums.push_back(-2);
-    nums.push_back(1);
-    nums.push_back(-3);
-    nums.push_back(4);
-    nums.push_back(2);
-    nums.push_back(1);
-    nums.push_back(-5);
-    nums.push_back(4);
+    vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
 
     cout << "vector is : " ;
     for(int val:nums){
--- vectors/tempCodeRunnerFile.cpp
+++ vectors/tempCodeRunnerFile.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <limits>
 using namespace std;
 
+int sumOfSubarrays(vector<int>& nums){
+    int maxSum = 0; 
+    int currentSum =0;
+
+    for (int val: nums){
+        currentSum += val;
+        maxSum = max(currentSum , maxSum);
+        
+        if ( currentSum < 0){
+            currentSum = 0;
+        }
+        
+    }
+    return maxSum;
+}
 int main(){
-    vector<int> vec = {1,2,3,4,5};
+    vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
 
-    vector<int> :: iterator it;
-    for( it = vec.begin(); it!=vec.end(); it++){ // forward loop
-        cout << *it << " ";
+    cout << "vector is : " ;
+    for(int val:nums){
+        cout<< val << " ";
     }
     cout << endl;
 
-    // vector<int> ::reverse_iterator i;
-    for(auto i = vec.rbegin(); i!=vec.rend(); i++){ // backward loop
-        cout << *i << " ";
-    }
-    cout << endl;
-
-    
+    cout << "Maximum Sum of subarray is : " << sumOfSubarrays(nums);
     return 0;
 }
